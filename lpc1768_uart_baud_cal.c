3b0f6dd85cbea7aec99e1456da97d6e90e20dabd
charlie lee <charlie388@@gmail.com>
2017-12-30 18:01:07 +0800

    fix segmentation fault issue in linux (Ubuntu 16.04.03)
    It modify a lot. The main cause is "index" variable which is uint8_t type.
    When index=0 and index--, it will cause segemntation fault.
    
    This program shared link to libm.so.6.
    It need gcc add option -lm at end.
    
    From previous version:
    When I static link to library program run well.
    When I shared link to library program run segmentation fault.

--- lpc1768_uart_baud_cal.c
+++ lpc1768_uart_baud_cal.c
@@ -20,23 +20,25 @@ uint32_t gcd(uint32_t a, uint32_t b) {
 }
 
 int main() {
-    uint8_t DIVADDVAL, MULVAL, index;
-    double tmpFractional;
+    uint8_t index;
     uint32_t pclk, baud, divider;
     
     //Due to mintty buffered for a pipe
     //We can use below setting or append each printf() with fflush(stdout)  
     setvbuf(stdout, 0, _IONBF, 0);
 
-    index=0;
-    for( DIVADDVAL=1; DIVADDVAL<=14; DIVADDVAL++) {
-        for( MULVAL=2; MULVAL<=15; MULVAL++) {
-            if(DIVADDVAL<MULVAL) {
-                if( gcd(DIVADDVAL, MULVAL)==1 ) {
-                    param[index].DIVADDVAL = DIVADDVAL;
-                    param[index].MULVAL = MULVAL;
-                    //param[index].FD = ((double)MULVAL)/((double)(MULVAL+DIVADDVAL));
-                    param[index].FD = ((double)(MULVAL+DIVADDVAL))/((double)MULVAL);
+    param[0].DIVADDVAL = 0;
+    param[0].MULVAL = 1;
+    param[0].FD = 1.0;
+    index=1;
+    for( uint8_t divaddval=1; divaddval<=14; divaddval++) {
+        for( uint8_t mulval=2; mulval<=15; mulval++) {
+            if(divaddval<mulval) {
+                if( gcd(divaddval, mulval)==1 ) {
+                    param[index].DIVADDVAL = divaddval;
+                    param[index].MULVAL = mulval;
+                    //param[index].FD = ((double)mulval)/((double)(mulval+divaddval));
+                    param[index].FD = ((double)(mulval+divaddval))/((double)mulval);
                     index++;
                 }
             }
@@ -46,23 +48,25 @@ int main() {
     param[index].DIVADDVAL = 0xFF;
     param[index].MULVAL = 0xFF;
 
-    for(int i=0; i<index; i++) {
-        for(int j=i; j<index; j++) {
+    //sorting the param table base on FD
+    for(uint8_t i=0; i<index; i++) {
+        for(uint8_t j=i; j<index; j++) {
             if( param[i].FD > param[j].FD ) {
-                DIVADDVAL = param[i].DIVADDVAL;
-                MULVAL = param[i].MULVAL;
-                tmpFractional = param[i].FD;
+                uint8_t divaddval, mulval;
+                double tmpFD;
+                divaddval = param[i].DIVADDVAL;
+                mulval = param[i].MULVAL;
+                tmpFD = param[i].FD;
                 param[i].DIVADDVAL = param[j].DIVADDVAL;
                 param[i].MULVAL = param[j].MULVAL;
                 param[i].FD = param[j].FD;
-                param[j].DIVADDVAL = DIVADDVAL;
-                param[j].MULVAL = MULVAL;
-                param[j].FD = tmpFractional;
+                param[j].DIVADDVAL = divaddval;
+                param[j].MULVAL = mulval;
+                param[j].FD = tmpFD;
             }
         }
     }
 
-    index=0;
     for(index=0; ; index++) {
         if(param[index].DIVADDVAL == 0xFF) break;
         printf("%3d. MULVAL %2d, DIVADDVAL %2d, FD %lf \n", index+1, param[index].MULVAL, param[index].DIVADDVAL, param[index].FD);
@@ -77,12 +81,12 @@ int main() {
     baud = 115200;
 */
     printf("PCLK=%dHZ, Baud=%d\n", pclk, baud);
-    printf("========================================================\n");
-    //printf("divider\t wantFD\t\t closeFD\t realBaud\t offset\t\n");
-    printf("DLM\t DLL\t DIVADDVAL\t MULVAL\t Baud\t offset\t error% \n");
+    printf("==============================================================================\n");
+    //printf("divider\t wantFD\t\t closeFD\t Baud\t\t offset\t \n");
+    printf("DLM\t DLL\t DIVADDVAL\t MULVAL\t Baud\t offset\t error%% \n");
 
-    // if we use ceil() the first divider need a fractional divider (FD) is probably 0.9XXX.
-    // if we use floor() the first divider need a (FD) is probably 1.XXXX.
+    // if we use ceil() the first divider want a fractional divider (FD) is probably 0.9XXX.
+    // if we use floor() the first divider want a (FD) is probably 1.XXXX.
     // The available (FD) value is between 1 and 2. 
     // If (second divider = first divider +1), the needed FD is smaller than 1
     // If (second divider = first divider -1), the needed FD is larger than 1
@@ -91,42 +95,39 @@ int main() {
     divider = (uint32_t)ceil( pclk / 16.0 / baud );
 
     while(divider>2) {
-        double minOffset, offset, error;
-        double wantFD, closeFD;
-        uint32_t realBaud;
+        double wantFD, minOffsetFD, offsetFD, error;
+        uint32_t realBaud, offsetBaud;
         
         wantFD = pclk / 16.0 / divider / baud;
         if( wantFD>2.0 ) break;
  
-        // Before we compare the param table we should always compare the 1 which mean NO fractional divider.
-        minOffset = fabs(wantFD - 1);
+        // The param[0].FD=1 which mean NO fractional divider.
+        minOffsetFD = fabs( wantFD - param[0].FD );
 
-        for(index=0; ; index++) {
+        for(index=1; ; index++) {
 
             if( (param[index].DIVADDVAL == 0xFF) || (param[index].MULVAL == 0xFF) ) {
                 index--;
                 break;
             }
 
-            offset = fabs ( param[index].FD - wantFD );
+            offsetFD = fabs ( wantFD - param[index].FD );
 
-            if( offset < minOffset ) {
-                minOffset = offset;
+            if( offsetFD < minOffsetFD ) {
+                minOffsetFD = offsetFD;
             } else {
-                offset = minOffset;
+                offsetFD = minOffsetFD;
                 index--;
                 break; 
             }
 
         }
 
-        if(index == 0xFF) { closeFD = 1.0; }
-        else { closeFD = param[index].FD; }
-
-        realBaud = (uint32_t)(pclk / 16.0 / divider / closeFD);
-        error = ( abs(baud-realBaud) * 1.0 ) / baud;
-        //printf("%d\t %lf\t %lf\t %d\t\t %d\t \n", divider, wantFD, closeFD, realBaud,  abs(baud-realBaud));
-        printf("%d\t %d\t %d\t\t %d\t %d\t %d\t %lf \n",divider/256, divider%256,  (index==-1)?0:param[index].DIVADDVAL, (index==-1)?0:param[index].MULVAL, realBaud, abs(baud-realBaud), error);
+        realBaud = (uint32_t)(pclk / 16.0 / divider / param[index].FD);
+        offsetBaud = abs(baud-realBaud);
+        error = (((double) offsetBaud ) / baud ) *100;
+        //printf("%d\t %lf\t %lf\t %d\t\t %d\t \n", divider, wantFD, param[index].FD, realBaud,  offsetBaud);
+        printf("%d\t %d\t %d\t\t %d\t %d\t %d\t %lf \n",divider/256, divider%256,  param[index].DIVADDVAL, param[index].MULVAL, realBaud, offsetBaud, error);
 
         divider--;
     }
